9ad91adb14a533d25b0fd46bb0aa7906a5ae848e
Yashodini Soysa <yasodinisoysa@gmail.com>
2025-04-02 13:02:04 +0530

    Add Json Serialization Configuration

---
+++ menu-management-service/src/main/java/com/restaurant/menu/config/JsonSerializationConfiguration.java
@@ -0,0 +1,22 @@
+package com.restaurant.menu.config;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JsonSerializationConfiguration {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        objectMapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+        return objectMapper;
+    }
+}
